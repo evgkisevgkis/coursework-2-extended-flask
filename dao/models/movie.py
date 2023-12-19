430354f05b1fde4a1463d77f2623bce6e9023187
Johnny <evgkisevgkis@gmail.com>
2023-12-19 13:00:07 +0500

    creating model and schema for movies

---
+++ dao/models/movie.py
@@ -0,0 +1,30 @@
+from setup_db import db
+from marshmallow import Schema, fields
+
+
+class Movie(db.Model):
+    """Model for movies"""
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))  # reference to genres table
+    genre = db.relationship('Genre')
+    director_id = db.Column(db.Integer, db.ForeignKey('directors.id'))  # reference to directors table
+    director = db.relationship('Directors')
+
+
+class MovieSchema(Schema):
+    """Schema for movies"""
+
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+
