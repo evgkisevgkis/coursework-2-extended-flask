ad840882ee64db0922fb564a7c7f281e10bfd0d0
Johnny <evgkisevgkis@gmail.com>
2023-12-19 12:45:33 +0500

    creating model and schema for directors

---
+++ dao/models/director.py
@@ -0,0 +1,17 @@
+from setup_db import db
+from marshmallow import Schema, fields
+
+
+class Director(db.Model):
+    """Model for directors"""
+    __tablename__ = 'directors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class DirectorSchema(Schema):
+    """Schema for directors"""
+
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
