622d887cdd895d27dd14a93592350bc9504deb4c
Johnny <evgkisevgkis@gmail.com>
2023-12-19 12:21:19 +0500

    creating model and schema for genres

---
+++ dao/models/genre.py
@@ -0,0 +1,17 @@
+from setup_db import db
+from marshmallow import Schema, fields
+
+
+class Genre(db.Model):
+    """Model for genres"""
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class GenreSchema(Schema):
+    """Schema for genres"""
+
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
