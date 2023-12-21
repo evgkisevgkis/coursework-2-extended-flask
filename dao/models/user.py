5fe98535c357a75d0fff3a8f5b72ce133200a7a0
Johnny <evgkisevgkis@gmail.com>
2023-12-21 14:16:09 +0500

    creating model and schema for users

---
+++ dao/models/user.py
@@ -0,0 +1,25 @@
+from setup_db import db
+from marshmallow import Schema, fields
+
+
+class User(db.Model):
+    """Model for users"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    favorite_genre = db.Column(db.String)
+
+
+class UserSchema(Schema):
+    """Schema for users"""
+
+    id = fields.Int(dump_only=True)
+    email = fields.Str()
+    password = fields.Str()
+    name = fields.Str()
+    surname = fields.Str()
+    favorite_genre = fields.Str()
