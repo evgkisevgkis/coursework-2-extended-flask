402d88f875f2dc88b92d39cce2334916d2b1c01f
Johnny <evgkisevgkis@gmail.com>
2023-12-21 14:35:16 +0500

    creating DAO for directors

---
+++ dao/director.py
@@ -0,0 +1,32 @@
+from dao.models.director import Director
+
+
+class DirectorDAO:
+    """DAO for directors, CRUD operations"""
+    def __init__(self, session):
+        self.session = session
+
+    def get_one(self, did):
+        return self.session.query(Director).get(did)
+
+    def get_all(self):
+        return self.session.query(Director).all()
+
+    def create(self, data):
+        new_director = Director(**data)
+        self.session.add(new_director)
+        self.session.commit()
+        return new_director
+
+    def update(self, data):
+        director = self.get_one(data.get('id'))
+        director.name = data.get('name')
+        self.session.add(director)
+        self.session.commit()
+        return director
+
+    def delete(self, did):
+        director = self.get_one(did)
+        self.session.delete(director)
+        self.session.commit()
+        return director
